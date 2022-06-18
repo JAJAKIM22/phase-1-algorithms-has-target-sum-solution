5d0852a4e576eeacbe33c5e72efb559ab8282ecb
Peter Kimani <peterjajakim@gmail.com>
2022-06-18 08:54:33 +0300

    commit tree 3217d598eed622b416932a28df201436201cda16

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++)
+     {
+      //if the sum  does not equal to zero return false
+     const sum = target - array[i];
+      //iterate through the array
+      for (let j = i + 1; j < array.length; j++)
+       {
+        // if the target number is equal to the sum of the two umbers in the array return true
+        if (array[j] === sum) return true;
+      }
+    }
+  
+   return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Runtime: O(n^2)
+  function findPair(laundry) {
+  // look through each item in the pile
+  for (let i = 0; i < laundry.length; i++) {
+    // look through the rest of the pile
+    for (let j = i + 1; j < laundry.length; j++) {
+      // check if it matches the first sock
+      if (laundry[i] === laundry[j]) {
+        return [laundry[i], laundry[j]];
+      }
+    }
+  }
+}
+
+findPair(["sock 5", "sock 2", "sock 1", "sock 3", "sock 1"]);
 */
 
 /* 
   Add your pseudocode here
+  take one number from the array
+  sum it to one other number  in the array
+    if they total sum matches a number from the pile, return true 
+    else false
 */
 
 /*
   Add written explanation of your solution here
+  take one number from the array
+  sum it to one other number  in the array
+    if they total sum matches a number from the pile, return true 
+    else false
 */
 
 // You can run `node index.js` to view these console logs
